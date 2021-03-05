bc4d8d6dbf79216392193acc2dc68d5c07675877
sukhrajbrar <sukhrajbrar15@yahoo.com>
2021-03-05 11:50:53 -0700

    d

--- automation.py
+++ automation.py
@@ -4,6 +4,6 @@ import subprocess
 for i in range(30):
     command = 'sudo knockknock -p 34 spaserver'
     subprocess.run(command.split(), shell=False)
-    time.sleep(10)
+    time.sleep(33)
 
 print("30 iterations done")
